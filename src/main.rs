4ed89bef054068a4763ccda6cbb543fc19caabce
MrMcEpic <45011453+MrMcEpic@users.noreply.github.com>
2019-03-01 13:03:43 -0500

    Updated syntax

--- src/main.rs
+++ src/main.rs
@@ -2,132 +2,124 @@ use rand::Rng;
 use std::{io, thread::sleep, time::Duration};
 
 mod text;
+use text::to_title;
 
-fn to_title(s1: &str) -> String{
-    let mut v: Vec<char> = s1.chars().collect();
-    v[0] = v[0].to_uppercase().nth(0).unwrap();
-    let s2: String = v.into_iter().collect();
-    s2
+fn again() {
+	println!("Play Again? Y/N");
+	let choice = loop {
+		let mut choice = String::new();
+
+		io::stdin()
+			.read_line(&mut choice)
+			.expect("Failed to read line");
+
+		choice = choice.trim().to_ascii_lowercase();
+
+		if choice == "y" || choice == "n" {
+			break choice;
+		} else if choice == "yes" {
+			choice = "y".to_string();
+			break choice;
+		} else if choice == "no" {
+			choice = "n".to_string();
+			break choice;
+		}
+	};
+	if choice == "y" {
+		game();
+	} else {
+		println!("Press enter to close");
+		let mut _enter = String::new();
+		io::stdin().read_line(&mut _enter).expect("Failed");
+	}
 }
 
-fn again(){
-    println!("Play Again? Y/N");
-    let choice = loop{
-        let mut choice = String::new();
-
-        io::stdin().read_line(&mut choice)
-            .expect("Failed to read line");
-        
-        choice = choice.trim().to_ascii_lowercase();
-
-       if choice == "y" || choice == "n"{
-           break choice;
-       }else if choice == "yes"{
-            choice = "y".to_string();
-            break choice;
-       }else if choice == "no"{
-            choice = "n".to_string();
-            break choice;
-       }
-    };
-    if choice == "y"{
-        game();
-    }else{
-        println!("Press enter to close");
-        let mut _enter = String::new();
-        io::stdin().read_line(&mut _enter)
-            .expect("Failed");
-    }
-}
-
-fn game(){
-    let user = choice_input();
-    println!("You chose {}!", to_title(&user));
-    sleep(Duration::from_millis(500));
-    let comp = computer_choice();
-    println!("Computer chose {}!", to_title(&comp));
-    sleep(Duration::from_millis(300));
-    let mut result = logic(&user, &comp);
-        let you = if result == "draw"{
-            result = to_title(&result);
-            ""
-        }else{
-            "You "
-        };
-        let exclaim = if result == "win"{
-            "!"
-        }else{
-            ""
-        };
-    println!("{}{}{}", you, result, exclaim);
-    again();
+fn game() {
+	let user = choice_input();
+	println!("You chose {}!", to_title(&user));
+	sleep(Duration::from_millis(500));
+	let comp = computer_choice();
+	println!("Computer chose {}!", to_title(&comp));
+	sleep(Duration::from_millis(300));
+	let mut result = logic(&user, &comp);
+	let you = if result == "draw" {
+		result = to_title(&result);
+		None
+	} else {
+		Some("You ")
+	};
+	let exclaim = if result == "win" { Some("!") } else { None };
+	println!(
+		"{}{}{}",
+		if let Some(i) = you { i } else { "" },
+		result,
+		if let Some(i) = exclaim { i } else { "" }
+	);
+	again();
 }
 
 fn main() {
-    text::text();
-    game();
+	text::text();
+	game();
 }
 
 fn logic(user: &str, comp: &str) -> String {
-    let mut return_val = "";
-    if user == comp {
-        return_val = "draw";
-    }else{
-        match user{
-            "rock" => {
-                if comp == "paper"{
-                    return_val = "lose";
-                }else{
-                    return_val = "win";
-                }
-            },
-            "paper" => {
-                 if comp == "scissors"{
-                    return_val = "lose";
-                }else{
-                    return_val = "win";
-                }
-            },
-            "scissors" => {
-                if comp == "rock"{
-                    return_val = "lose";
-                }else{
-                    return_val = "win";
-                }
-            },
-            _ =>{
-                println!("Error")
-            }
-        }
-    }
-    return_val.to_string()
+	let return_val = if user == comp {
+		"draw"
+	} else {
+		match user {
+			"rock" => {
+				if comp == "paper" {
+					"lose"
+				} else {
+					"win"
+				}
+			}
+			"paper" => {
+				if comp == "scissors" {
+					"lose"
+				} else {
+					"win"
+				}
+			}
+			"scissors" => {
+				if comp == "rock" {
+					"lose"
+				} else {
+					"win"
+				}
+			}
+			_ => "",
+		}
+	};
+	return_val.to_string()
 }
 
-fn computer_choice() -> String{
-    let comp_num = rand::thread_rng().gen_range(1,4);
-    let mut _comp_val = "";
-    match comp_num {
-        1 => _comp_val = "rock",
-        2 => _comp_val = "paper",
-        3 => _comp_val = "scissors",
-        _ => _comp_val = "broken",
-    };
-    _comp_val.to_string()
+fn computer_choice() -> String {
+	let comp_num = rand::thread_rng().gen_range(1, 4);
+	let _comp_val = match comp_num {
+		1 => "rock",
+		2 => "paper",
+		3 => "scissors",
+		_ => "broken",
+	};
+	_comp_val.to_string()
 }
 
-fn choice_input() -> String{
-    loop{
-        println!("Rock/Paper/Scissors?");
+fn choice_input() -> String {
+	loop {
+		println!("Rock/Paper/Scissors?");
 
-        let mut choice = String::new();
+		let mut choice = String::new();
 
-        io::stdin().read_line(&mut choice)
-            .expect("Failed to read line");
-        
-        choice = choice.trim().to_ascii_lowercase();
+		io::stdin()
+			.read_line(&mut choice)
+			.expect("Failed to read line");
 
-        if choice == "rock" || choice == "paper" || choice == "scissors" {
-            break choice; //returns choice to loop which then returns to function call
-        }
-    }//no ; here means it returns loop value to where it was called
+		choice = choice.trim().to_ascii_lowercase();
+
+		if choice == "rock" || choice == "paper" || choice == "scissors" {
+			break choice; //returns choice to loop which then returns to function call
+		}
+	} //no ; here means it returns loop value to where it was called
 }
--- src/text.rs
+++ src/text.rs
@@ -1,8 +1,8 @@
-use wincolor::{Console, Color, Intense};
-use winconsole::console;
 use std::{thread::sleep, time::Duration};
- 
-pub fn text(){
+use wincolor::{Color, Console, Intense};
+use winconsole::console;
+
+pub fn text() {
 	let mut con = Console::stdout().unwrap();
 	console::set_title("Rock Paper Scissors").unwrap();
 
@@ -10,13 +10,21 @@ pub fn text(){
 		con.fg(Intense::Yes, Color::Cyan).unwrap();
 		sleep(Duration::from_millis(500));
 
-		println!("{holder:^width$}", width=w, holder="~-==Rock Paper Scissors==-~");
+		println!(
+			"{holder:^width$}",
+			width = w,
+			holder = "~-==Rock Paper Scissors==-~"
+		);
 
 		con.reset().unwrap();
 		sleep(Duration::from_millis(1000));
 		con.fg(Intense::Yes, Color::Green).unwrap();
 
-		println!("{holder:^width$}", width=w, holder="~-==Now built in Rust!==-~");
+		println!(
+			"{holder:^width$}",
+			width = w,
+			holder = "~-==Now built in Rust!==-~"
+		);
 
 		sleep(Duration::from_millis(1000));
 		con.reset().unwrap();
@@ -24,15 +32,30 @@ pub fn text(){
 		con.fg(Intense::Yes, Color::Cyan).unwrap();
 		sleep(Duration::from_millis(500));
 
-		println!("{holder:^width$}", width=110, holder="~-==Rock Paper Scissors==-~");
+		println!(
+			"{holder:^width$}",
+			width = 110,
+			holder = "~-==Rock Paper Scissors==-~"
+		);
 
 		con.reset().unwrap();
 		sleep(Duration::from_millis(1000));
 		con.fg(Intense::Yes, Color::Green).unwrap();
 
-		println!("{holder:^width$}", width=110, holder="~-==Now built in Rust!==-~");
+		println!(
+			"{holder:^width$}",
+			width = 110,
+			holder = "~-==Now built in Rust!==-~"
+		);
 
 		sleep(Duration::from_millis(1000));
 		con.reset().unwrap();
-	}	
+	}
+}
+
+pub fn to_title(s1: &str) -> String {
+	let mut v: Vec<char> = s1.chars().collect();
+	v[0] = v[0].to_uppercase().nth(0).unwrap();
+	let s2: String = v.into_iter().collect();
+	s2
 }
